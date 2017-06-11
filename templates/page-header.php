26ad069383da0ae18b546c9ee9db8b8159b697d0
Archie <archie.jereos@gmail.com>
2017-06-11 13:06:37 +0800

    Added meta on single/post

--- templates/page-header.php
+++ templates/page-header.php
@@ -3,6 +3,6 @@
 <header class="page-header">
 	<div class="container">
   	<h1><?= Titles\title(); ?></h1>
-		<?php get_template_part('templates/entry-meta'); ?>
+		<?php if( is_single() ) { get_template_part('templates/entry-meta'); } ?>
   </div>
 </header>
